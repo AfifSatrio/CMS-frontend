4a6e4978f145e61752219f41689e5ff2bdf60fb3
AfifSatrio <afifsatria2108@gmail.com>
2025-10-10 21:19:53 +0700

    update warna + font

---
+++ app/(client)/Organizational/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrganizationalPage = () => {
+  return (
+    <div>OrganizationalPage</div>
+  )
+}
+
+export default OrganizationalPage
--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -2,6 +2,8 @@ import { type Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import localFont from 'next/font/local';
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
 
 const NunitoSans = localFont({
   src: '../fonts/NunitoSans.woff2',
@@ -23,6 +25,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={`${NunitoSans.variable} antialiased`}>
+          <Header />
+          <Sidebar />
           { children }
         </body>
       </html>
